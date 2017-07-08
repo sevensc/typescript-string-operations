89a33a750befa877af847a87985e88646d37bb70
flowoj <florian@wojtenek.de>
2017-07-08 02:12:29 +0200

    Build hinzugefügt und noImplicitAny aktiviert

--- dist/source.js
+++ dist/source.js
@@ -1,15 +1,8 @@
-/// <reference path="../definitions/jquery.d.ts" />
-
-declare var $: JQueryStatic;
-
 export class String {
-    public static Empty: string = "";
-
-    public static IsNullOrWhiteSpace(value: string): boolean {
+    static IsNullOrWhiteSpace(value) {
         try {
             if (value == null || value == 'undefined')
                 return true;
-
             return value.toString().replace(/\s/g, '').length < 1;
         }
         catch (e) {
@@ -17,13 +10,11 @@ export class String {
             return false;
         }
     }
-
-    public static Join(delimiter: string, ...args: string[]): string {
+    static Join(delimiter, ...args) {
         try {
             if ($.isArray(args[0]) || args[0] === typeof Array) {
                 var tempString = String.Empty;
                 var count = 0;
-
                 for (var i = 0; i < args[0].length; i++) {
                     var current = args[0][i];
                     if (i < args[0].length - 1)
@@ -31,7 +22,6 @@ export class String {
                     else
                         tempString += current;
                 }
-
                 return tempString;
             }
             else if (typeof args[0] == 'object') {
@@ -44,10 +34,8 @@ export class String {
                         tempString += $(this).text();
                     count++;
                 });
-
                 return tempString;
             }
-
             return String.join(delimiter, args);
         }
         catch (e) {
@@ -55,16 +43,14 @@ export class String {
             return String.Empty;
         }
     }
-
-    public static Format(format: string, ...args: string[]): string {
+    static Format(format, ...args) {
         try {
-            return format.replace(/{(\d+(:\w*)?)}/g, function (match, i) { //0
+            return format.replace(/{(\d+(:\w*)?)}/g, function (match, i) {
                 var s = match.split(':');
                 if (s.length > 1) {
                     i = i[0];
-                    match = s[1].replace('}', ''); //U
+                    match = s[1].replace('}', '');
                 }
-
                 var arg = String.parsePattern(match, args[i]);
                 return typeof arg != 'undefined' && arg != null ? arg : String.Empty;
             });
@@ -74,11 +60,9 @@ export class String {
             return String.Empty;
         }
     }
-
-    private static parsePattern(match: 'L' | 'U' | 'd' | 's' | 'n' | string, arg: string): string {
+    static parsePattern(match, arg) {
         if (arg == null || arg == undefined)
             return arg;
-
         switch (match) {
             case 'L':
                 arg = arg.toLowerCase();
@@ -90,39 +74,33 @@ export class String {
                 var splitted = arg.split('-');
                 if (splitted.length <= 1)
                     return arg;
-
                 var day = splitted[splitted.length - 1];
                 var month = splitted[splitted.length - 2];
                 var year = splitted[splitted.length - 3];
                 day = day.split('T')[0];
                 day = day.split(' ')[0];
-
                 arg = day + '.' + month + '.' + year;
                 break;
             case 's':
                 var splitted = arg.replace(',', '').split('.');
                 if (splitted.length <= 1)
                     return arg;
-
                 let times = splitted[splitted.length - 1].split(' ');
                 let time = splitted[0];
                 if (times.length > 1)
                     time = time[time.length - 1];
-
                 var year = splitted[splitted.length - 1].split(' ')[0];
                 var month = splitted[splitted.length - 2];
                 var day = splitted[splitted.length - 3];
-
                 arg = year + "-" + month + "-" + day;
                 if (time.length > 1)
                     arg += "T" + time;
                 else
                     arg += "T" + "00:00:00";
                 break;
-            case 'n': //Tausender Trennzeichen
+            case 'n':
                 if (isNaN(parseInt(arg)) || arg.length <= 3)
                     break;
-
                 arg = arg.toString();
                 var mod = arg.length % 3;
                 var output = (mod > 0 ? (arg.substring(0, mod)) : String.Empty);
@@ -137,22 +115,18 @@ export class String {
             default:
                 break;
         }
-
         return arg;
     }
-
-    private static join(delimiter:string, args:string[]): string {
+    static join(delimiter, args) {
         var temp = String.Empty;
         for (var i = 0; i < args.length; i++) {
             if (String.IsNullOrWhiteSpace(args[i]) || (typeof args[i] != "number" && typeof args[i] != "string"))
                 continue;
-
             var arg = "" + args[i];
             temp += arg;
             for (var i2 = i + 1; i2 < args.length; i2++) {
                 if (String.IsNullOrWhiteSpace(args[i2]))
                     continue;
-
                 temp += delimiter;
                 i = i2 - 1;
                 break;
@@ -161,24 +135,22 @@ export class String {
         return temp;
     }
 }
-
+String.Empty = "";
 export class StringBuilder {
-    public Values : string[] = [];
-
-    constructor(value: string = String.Empty) {
+    constructor(value = String.Empty) {
+        this.Values = [];
         this.Values = new Array(value);
     }
-
-    public ToString() {
+    ToString() {
         return this.Values.join('');
     }
-    public Append(value: string) {
+    Append(value) {
         this.Values.push(value);
     }
-    public AppendFormat(value: string, ...args : string[]) {
+    AppendFormat(value, ...args) {
         this.Values.push(String.Format(value, ...args));
     }
-    public Clear() {
+    Clear() {
         this.Values = [];
     }
 }
